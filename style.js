54b1b8b4e845a7d33a2a848b508fc5ee9d350a35
Eli Sturgeon <eli.sturgeon3@gmail.com>
2022-10-19 13:34:12 -0400

    Completed console rock paper scissor game Contains computer selector, player selector And win conditions Still needs a way to check for errors And needs a user interface in html

--- style.js
+++ style.js
@@ -1,7 +1,38 @@
-const choices = ["Rock", "Paper", "Scissors"];
+const choices = ["rock", "paper", "scissors"];
+const playerSelection = playerChoice();
+const computerSelection = getComputerChoice();
+const winner = winConditions();
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
 
+function playerChoice() {
+    let input = prompt("Rock, Paper, or Scissors?", "");
+    input = input.toLowerCase();
+    return(input);
+}
+
+
+function winConditions() {
+    if (computerSelection === playerSelection) {
+        return ("Tie");
+    } else if (computerSelection === "scissors" && playerSelection === "paper") {
+            return ("Computer wins!");
+        } else if (computerSelection === "paper" && playerSelection === "rock") {
+            return ("Computer Wins!");
+        } else if (computerSelection === "rock" && playerSelection === "scissors") {
+            return ("Computer wins!");
+         } else {
+            return("You win!");
+        } 
+    }
+
+function playRound() {
+    console.log(playerSelection);
+    console.log(computerSelection);
+    console.log(winner);
+}
+
+playRound();
