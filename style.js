87dd09d59a535f3033e4393255dcfc330541fcf4
Eli Sturgeon <eli.sturgeon3@gmail.com>
2022-10-21 10:20:33 -0400

    Final iteration of rock paper scissors Added 5 round capability Added tally system to determine winner

--- style.js
+++ style.js
@@ -1,7 +1,8 @@
 const choices = ["rock", "paper", "scissors"];
-const playerSelection = playerChoice();
-const computerSelection = getComputerChoice();
-const winner = winConditions();
+//const playerSelection = playerChoice();
+//const computerSelection = getComputerChoice();
+//let playerScore = 0;
+//let computerScore = 0;
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
@@ -15,24 +16,53 @@ function playerChoice() {
 }
 
 
-function winConditions() {
+function winConditions(playerSelection, computerSelection) {
     if (computerSelection === playerSelection) {
-        return ("Tie");
+        return "Tie";
     } else if (computerSelection === "scissors" && playerSelection === "paper") {
-            return ("Computer wins!");
+            return ("Computer");
         } else if (computerSelection === "paper" && playerSelection === "rock") {
-            return ("Computer Wins!");
+            return ("Computer");
         } else if (computerSelection === "rock" && playerSelection === "scissors") {
-            return ("Computer wins!");
+            return ("Computer");
          } else {
-            return("You win!");
+            return("Player");
         } 
-    }
-
-function playRound() {
-    console.log(playerSelection);
-    console.log(computerSelection);
-    console.log(winner);
 }
 
-playRound();
+function playRound(playerSelection, computerSelection) {
+    //console.log(playerChoice());
+    //console.log(getComputerChoice());
+    const result = winConditions(playerSelection, computerSelection);
+    if (result === "Tie") {
+        return ("It is a tie!");
+    } else if (result === "Computer") {
+        return ("Computer wins!");
+    } else {
+        if (result === "Player") {
+            return ("You win!");
+        }
+    } 
+}
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let tieScore = 0;
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = playerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        if (winConditions(playerSelection, computerSelection) === "Computer") {
+            computerScore++;
+        } else if (winConditions(playerSelection, computerSelection) === "Player") {
+            playerScore++;
+        } else tieScore++;
+    } if (playerScore > computerScore && tieScore) {
+        console.log("You win!");
+    } else if (computerScore > playerScore && tieScore) {
+        console.log("Computer wins!");
+    } else console.log("No winner!");
+}
+
+game();
